ec0db65b87fb977da6ea40b4b8db3a647d22d7eb
Runbo Zhao <rzhao03@syr.edu>
2018-12-04 15:25:07 -0500

    deploy change.

--- src/App.js
+++ src/App.js
@@ -300,7 +300,7 @@ class App extends Component {
     this.setState({ newKanbanFormData });
     kanbanService.createNewKanban(newKanbanFormData);
     this.closeNewKanbanModal();
-    window.location.reload(); // refresh the page
+    window.location = "/kanban"; // refresh the page
   };
 
   /**the new kanban cancel button handler */
